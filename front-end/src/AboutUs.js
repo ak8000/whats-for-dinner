fca59af6419d5e00555d87f169b26cb618b41d25
ak8000 <ak8000@nyu.edu>
2023-05-25 10:52:59 -0400

    fixes

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -7,11 +7,11 @@ const AboutUs = props => {
         <div className="AboutUs">
             <h1>About Us</h1>
             <p>Thanks for checking out our app!</p>
-            <p>We are a team of college students who wanted to build something that would help us eat healthier and improve our cooking skills at the same time.</p>
-            <p>However, the reason we developed this specefic app is to be able to cook with limited ingredients.</p>
+            <p>I am a college student who wanted to build something that would help me eat healthier and improve my cooking skills at the same time.</p>
+            <p>However, the reason I developed this specefic app is to be able to cook with limited ingredients.</p>
             <p>The goal of this app is to be able to find recipes based on the limited number of ingredients most college students possess.</p>
-            <p>We all have a passion for cooking and our goal is that by using this app, you can develop one too</p>
-            <p>If you have any suggestions, comments, or just want to get in contact with us, please do so below:</p>
+            <p>Because I have a passion for cooking and my goal is that by using this app, you can develop one too</p>
+            <p>If you have any suggestions, comments, or just want to get in contact with me, please do so below:</p>
             <li className="ContactLink"><Link to="/contactus">Contact Us</Link></li>
         </div>
     );
